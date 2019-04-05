f1caaedf85770a385a6841a03bfa88a2fcff593c
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-04-05 14:43:58 +0300

    Add files via upload

---
+++ JSONClasses/JSONClasses_Tests/CharacterFacts.cs
@@ -0,0 +1,17 @@
+﻿using JSONClasses;
+using Xunit;
+
+namespace JSONClasses_Tests
+{
+    public class CharacterFacts
+    {
+        [Fact]
+        public void Check_Returned_String_Empty()
+        {
+            IMatch match = new SuccessMatch("");
+            Assert.True(match.Succes());
+            Assert.Equal("", match.RemainingText());
+        }
+
+    }
+}
