bc5240a30c25a234b64d226c524c8f577afc5af7
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-04-08 00:11:51 +0300

    Classes(up to Sequence.cs)

---
+++ JSONClasses/Sequance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONClasses
+{
+    public class Sequance : IPattern
+    {
+        private readonly IPattern[] patterns;
+
+        public Sequance(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            foreach (var digit in patterns)
+            {
+                IMatch match = digit.Match(text);
+                if (match.Succes())
+                    text = match.RemainingText();
+                else
+                    return new FailedMatch(text);
+            }
+            return new SuccessMatch(text);
+        }
+
+    }
+}
