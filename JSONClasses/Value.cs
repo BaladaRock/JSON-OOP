c2b6abcc01c5dbbae937f89b8ade239bda39a7ab
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-04-18 21:16:20 +0300

    Add files via upload

---
+++ JSONClasses/Value.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONClasses
+{
+     public class Value : IPattern
+     {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var value = new Choice(
+                new String(),
+                new Number(),
+                new Text("true"),
+                new Text("false"),
+                new Text("null")
+            );
+
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(','), whitespace);
+            var element = new Sequence(whitespace, value, whitespace);
+           
+            var array = new Sequence(new Character('['),
+                new List(element,separator),
+                new Character(']')
+            );
+
+            var objectSeparator = new Sequence(whitespace, new Character(':'), whitespace);
+            var objectString = new Sequence(whitespace, new String(), whitespace);
+            var members = new Sequence(objectString, objectSeparator, element);
+
+            var jsonObject = new Sequence(new Character('{'),
+               new List(members, separator),
+               new Character('}')
+           );
+
+            value.Add(array);
+            value.Add(jsonObject);
+
+            pattern = new Sequence(new Choice(value, array, jsonObject));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
